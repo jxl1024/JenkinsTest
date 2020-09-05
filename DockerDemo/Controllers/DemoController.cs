3d041ca3c6ae3ed55a6f1deb71ed8d9f28b78df0
jiangxiaoliang <jiangxiaoliang1024@163.com>
2020-09-05 09:40:11 +0800

    submit code

---
+++ DockerDemo/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "测试Jenkins发布到docker";
+        }
+    }
+}
